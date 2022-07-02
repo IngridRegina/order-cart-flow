6c6a641552ea41d60558b9afc6eb0d980700bd7f
Ingrid-Regina Vähi <ingrid.v2hi@gmail.com>
2022-07-02 15:17:49 +0300

    feat: add product app to installed apps

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
-    'rest_framework.authtoken',
     'drf_spectacular',
+    'products',
 ]
 
 MIDDLEWARE = [
